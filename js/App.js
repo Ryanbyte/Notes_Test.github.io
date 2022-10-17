fc04e8c616f20ca06ebb2a1a47b7411d82d8f6cc
Ryan <107238211+Ryanbyte@users.noreply.github.com>
2022-10-17 12:16:41 +0600

    Create App.js

---
+++ js/App.js
@@ -0,0 +1,101 @@
+import NotesView from "./NotesView.js";
+import NotesAPI from "./NotesAPI.js";
+
+export default class App {
+    constructor(root) {
+        this.notes = [];
+        this.activeNote = null;
+        this.view = new NotesView(root, this._handlers());
+
+        this._refreshNotes();
+    }
+
+    _refreshNotes() {
+        const notes = NotesAPI.getAllNotes();
+
+        this._setNotes(notes);
+
+        if (notes.length > 0) {
+            this._setActiveNote(notes[0]);
+        }
+    }
+
+    _setNotes(notes) {
+        this.notes = notes;
+        this.view.updateNoteList(notes);
+        this.view.updateNotePreviewVisibility(notes.length > 0);
+    }
+
+    _setActiveNote(note) {
+        this.activeNote = note;
+        this.view.updateActiveNote(note);
+    }
+
+    _handlers() {
+        return {
+            onNoteSelect: noteId => {
+                const selectedNote = this.notes.find(note => note.id == noteId);
+                this._setActiveNote(selectedNote);
+            },
+            onNoteAdd: () => {
+                const newNote = {
+                    title: "New Note",
+                    body: "Take note..."
+                };
+
+                NotesAPI.saveNote(newNote);
+                this._refreshNotes();
+            },
+            onNoteEdit: (title, body) => {
+                NotesAPI.saveNote({
+                    id: this.activeNote.id,
+                    title,
+                    body
+                });
+
+                this._refreshNotes();
+            },
+            onNoteDelete: noteId => {
+                NotesAPI.deleteNote(noteId);
+                this._refreshNotes();
+            },
+        };
+    }
+}
+js/NotesAPI.js
+
+content_copy
+export default class NotesAPI {
+    static getAllNotes() {
+        const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+        });
+    }
+
+    static saveNote(noteToSave) {
+        const notes = NotesAPI.getAllNotes();
+        const existing = notes.find(note => note.id == noteToSave.id);
+
+        // Edit/Update
+        if (existing) {
+            existing.title = noteToSave.title;
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toISOString();
+        } else {
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
+    }
+
+    static deleteNote(id) {
+        const notes = NotesAPI.getAllNotes();
+        const newNotes = notes.filter(note => note.id != id);
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
+    }
+}
